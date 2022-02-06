ac6fec913aefc0ee4f41bdc58010eef062693df0
nbaltodano <nicholasbaltodano@gmail.com>
2022-02-06 10:27:57 -0600

    added handlers to custom servemux

--- main.go
+++ main.go
@@ -1,34 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/NicholasBaltodano/go-micro/handlers"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
+	helloHandler := handlers.NewHello(logger)
+	goodbyeHandler := handlers.NewGoodbye(logger)
 
-	http.HandleFunc("/favicon", doNothing)
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/goodbye", goodbye)
-	http.ListenAndServe("127.0.0.1:9090", nil)
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", helloHandler)
+	serveMux.Handle("/goodbye", goodbyeHandler)
+
+	serveMux.HandleFunc("/favicon", doNothing)
+
+	http.ListenAndServe("127.0.0.1:9090", serveMux)
 }
 
 func doNothing(w http.ResponseWriter, r *http.Request) {
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hello World !")
-
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "oops", http.StatusBadRequest)
-	}
-
-	fmt.Fprintf(w, "Hello %s", data)
-}
-
-func goodbye(w http.ResponseWriter, r *http.Request) {
-	log.Println("Goodbye world!")
 }
